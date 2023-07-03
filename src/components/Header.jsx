4440319e537e6e13003ab4d6fa873323f4a4150d
wendy <wendygalble@gmail.com>
2023-07-03 01:33:04 -0600

    simple header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <>
+      <nav className='navbar bg-body-tertiary'>
+        <div className='container-fluid'>
+          <span className='navbar-brand mb-0 h1'>REACT ISSUES</span>
+        </div>
+      </nav>
+    </>
+
+  )
+}
+
+export default Header
