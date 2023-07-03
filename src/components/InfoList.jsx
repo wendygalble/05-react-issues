c99ef9542c4f43a1f52992f21e6a259da22883c8
wendy <wendygalble@gmail.com>
2023-07-03 03:16:19 -0600

    botón

--- src/components/InfoList.jsx
+++ src/components/InfoList.jsx
@@ -1,13 +1,19 @@
-const InfoList = ({ id, title, url }) => {
+const InfoList = ({ id, title, node_id }) => {
   const handleIssueClick = () => {
-    const vinculo = 'html_url'
+    const link = 'html_url'
+
+    const encodedLink = encodeURI(link)
+
+    const endpoint = 'https://api.github.com/repos/facebook/react/issues'
+
+    window.open(endpoint)
   }
 
   return (
     <ul className='list-group'>
       <button className='vinculo' onClick={handleIssueClick}>{title}</button>
       <li className='list-group-item'>ID: {id}</li>
-      <li className='list-group-item'>{url}</li>
+      <li className='list-group-item'>Usuario:{node_id}</li>
     </ul>
 
   )
